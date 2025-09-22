cd93f89c9d236a862f044d02e81a4cf012529208
Raihan Khan <71407079+Raih9K@users.noreply.github.com>
2025-09-22 23:29:18 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { BasePage } from './base.page.js';
 import { AdminPage } from '../pages/adminpanel/admin.page.js';
 import { LoginPage } from './auth/login.page.js';
-import { DealsPage } from './deals/deals.page.js';
 import { BudgetPage } from './budgets/budget.page.js';
 
 export class AppPages {
+  
   constructor(page) {
     this.loginPage = new LoginPage(page);
     this.adminPage = new AdminPage(page);
